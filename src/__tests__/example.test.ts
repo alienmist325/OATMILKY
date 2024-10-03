a4ea4297709ee2ffae844d04071191e7f33ae9e1
Akesh Mistry <akesh.mistry@caplin.com>
2024-10-03 11:30:40 +0100

    add CoffeeShop, GoogleMaps and CoffeeShopLocator classes and headline test

---
+++ src/__tests__/example.test.ts
@@ -0,0 +1,84 @@
+class CoffeeShop {
+  private name: string;
+  private servesOATLY: boolean;
+  private priceUnderThreeFifty: boolean;
+
+  constructor(
+    name: string,
+    servesOATLY: boolean,
+    priceUnderThreeFifty: boolean
+  ) {
+    this.name = name;
+    this.servesOATLY = servesOATLY;
+    this.priceUnderThreeFifty = priceUnderThreeFifty;
+  }
+}
+
+class GoogleMaps {
+  constructor() {}
+
+  fetchShops(location: string, radiusInMetres: number): CoffeeShop[] {
+    return [];
+  }
+}
+
+class CoffeeShopLocator {
+  private maps;
+
+  constructor(maps: GoogleMaps) {
+    this.maps = maps;
+  }
+
+  getMatchingShops(location: string, radiusInMetres: number) {
+    const shopsInRadius = this.maps.fetchShops(location, radiusInMetres);
+    return shopsInRadius[0];
+  }
+}
+
+describe("Coffee Shop Locator", () => {
+  describe("when I have specified my location", () => {
+    describe("and I have entered my preferred search radius", () => {
+      it("should return Copper Coffee, given a certain set of coffee shops.", () => {
+        const blankStreetCoffee = new CoffeeShop(
+          "Blank Street Coffee",
+          false,
+          true
+        );
+
+        const coffeeMTowerOfLondon = new CoffeeShop(
+          "coffeeM Tower of London",
+          true,
+          false
+        );
+
+        const hagenEspresso = new CoffeeShop(
+          "Hagen Espresso Bar (Hagen Royal Exchange)",
+          false,
+          false
+        );
+
+        const copperCoffee = new CoffeeShop("Copper Coffee", true, true);
+
+        const mockShops = [
+          copperCoffee,
+          blankStreetCoffee,
+          coffeeMTowerOfLondon,
+          hagenEspresso,
+        ];
+
+        const location = "1, Sky Garden Walk, London EC3M 8AF";
+        const radius = 1000;
+
+        const maps = new GoogleMaps();
+        const mockFetchShops = jest.spyOn(maps, "fetchShops");
+        mockFetchShops.mockReturnValue(mockShops);
+
+        const coffeeShopLocator = new CoffeeShopLocator(maps);
+
+        expect(coffeeShopLocator.getMatchingShops(location, radius)).toBe(
+          copperCoffee
+        );
+      });
+    });
+  });
+});
